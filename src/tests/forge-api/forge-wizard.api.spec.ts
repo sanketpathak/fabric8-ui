532575ea7473120cb46e5070d2aee1fe3983fb08
Corinne Krych <corinnekrych@gmail.com>
2017-09-04 17:51:44 +0200

    Add steps to import wizard

--- src/tests/forge-api/forge-wizard.api.spec.ts
+++ src/tests/forge-api/forge-wizard.api.spec.ts
@@ -10,6 +10,14 @@ import {step_1_3_input} from './import-wizard/step_1_3_input';
 import {step_1_3_output} from 'tests/forge-api/import-wizard/step_1_3_output';
 import {step_1_2_input} from './import-wizard/step_1_2_input';
 import {step_1_2_output} from './import-wizard/step_1_2_output';
+import {step_2_1_output} from './import-wizard/step_2_1_output';
+import {step_2_1_input} from './import-wizard/step_2_1_input';
+import {step_2_2_output} from './import-wizard/step_2_2_output';
+import {step_2_2_input} from './import-wizard/step_2_2_input';
+import {step_3_1_input} from './import-wizard/step_3_1_input';
+import {step_3_1_output} from 'tests/forge-api/import-wizard/step_3_1_output';
+import {step_3_2_output} from './import-wizard/step_3_2_output';
+import {step_3_2_input} from './import-wizard/step_3_2_input';
 
 describe('Forge API tests:', () => {
   let mockLog: any;
@@ -28,6 +36,7 @@ describe('Forge API tests:', () => {
     mockLog = jasmine.createSpyObj('Logger', ['createLoggerDelegate']);
     mockAuthService = jasmine.createSpyObj('AuthenticationService', ['getToken', 'isLoggedIn']);
     mockApiLocatorService = jasmine.createSpyObj('ApiLocatorService', ['forgeApiUrl']);
+    provider.removeInteractions();
     TestBed.configureTestingModule({
       imports: [HttpModule],
       providers: [
@@ -47,6 +56,10 @@ describe('Forge API tests:', () => {
     });
     fabric8ForgeService = TestBed.get(Fabric8ForgeService);
   });
+
+  // afterEach(function() {
+  //   provider.removeInteractions();
+  // });
 
   it('Step_1_1 - import wizard: init', done => {
     // given
@@ -149,5 +162,190 @@ describe('Forge API tests:', () => {
         done();
       });
   });
+
+  it('Step_2_1 - import wizard: validate git repositories', done => {
+    // given
+    provider.addInteraction({
+      state: 'step2.1.validate',
+      uponReceiving: 'step2.1',
+      withRequest: {
+        method: 'POST',
+        path: '/forge/commands/fabric8-import-git/validate'
+      },
+      willRespondWith: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
+        body: step_2_1_output
+      }
+    }).then(() => done(), function(err) { console.log('failed'); done.fail(err); });
+    const log = () => { };
+    mockLog.createLoggerDelegate.and.returnValue(log);
+    mockAuthService.getToken.and.returnValue('SSO_TOKEN');
+    mockAuthService.isLoggedIn.and.returnValue(true);
+    // when
+    fabric8ForgeService.PostCommand('http://localhost:1234/forge/commands/fabric8-import-git/validate', step_2_1_input)
+      .subscribe((data: any) => {
+        // then
+        const response = data.payload.data;
+        expect(response.state.valid).toEqual(true);
+        expect(response.state.canMoveToNextStep).toEqual(true);
+        expect(response.state.canMoveToPreviousStep).toEqual(true);
+        expect(response.state.steps).toContain('io.fabric8.forge.generator.github.GithubImportPickOrganisationStep');
+        expect(response.state.steps).toContain('io.fabric8.forge.generator.github.GithubImportPickRepositoriesStep');
+        expect(response.state.steps).toContain('Obsidian: Configure Pipeline');
+        expect(response.state.steps).toContain('io.fabric8.forge.generator.kubernetes.CreateBuildConfigStep');
+        expect(response.messages.length).toEqual(0);
+        done();
+      });
+  });
+
+  it('Step_2_2 - import wizard: next git repositories', done => {
+    // given
+    provider.addInteraction({
+      state: 'step2.2.next',
+      uponReceiving: 'step2.2',
+      withRequest: {
+        method: 'POST',
+        path: '/forge/commands/fabric8-import-git/next'
+      },
+      willRespondWith: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
+        body: step_2_2_output
+      }
+    }).then(() => done(), function(err) { console.log('failed'); done.fail(err); });
+    const log = () => { };
+    mockLog.createLoggerDelegate.and.returnValue(log);
+    mockAuthService.getToken.and.returnValue('SSO_TOKEN');
+    mockAuthService.isLoggedIn.and.returnValue(true);
+    // when
+    fabric8ForgeService.PostCommand('http://localhost:1234/forge/commands/fabric8-import-git/validate', step_2_2_input)
+      .subscribe((data: any) => {
+        // then
+        const response = data.payload.data;
+        expect(response.metadata.name).toEqual('Obsidian: Configure Pipeline');
+        expect(response.state.valid).toEqual(true);
+        expect(response.state.canMoveToNextStep).toEqual(true);
+        expect(response.state.canMoveToPreviousStep).toEqual(true);
+        expect(response.state.steps).toContain('io.fabric8.forge.generator.github.GithubImportPickOrganisationStep');
+        expect(response.state.steps).toContain('io.fabric8.forge.generator.github.GithubImportPickRepositoriesStep');
+        expect(response.state.steps).toContain('Obsidian: Configure Pipeline');
+        expect(response.state.steps).toContain('io.fabric8.forge.generator.kubernetes.CreateBuildConfigStep');
+        expect(response.inputs[0].class).toEqual('UISelectOne');
+        expect(response.inputs[0].inputType).toEqual('org.jboss.forge.inputType.DEFAULT');
+        expect(response.inputs[0].valueType).toEqual('io.fabric8.forge.generator.pipeline.PipelineDTO');
+        expect(response.inputs[0].enabled).toEqual(true);
+        expect(response.inputs[0].required).toEqual(false);
+        expect(response.inputs[0].name).toEqual('pipeline');
+        expect(response.inputs[1].class).toEqual('UISelectOne');
+        expect(response.inputs[1].inputType).toEqual('org.jboss.forge.inputType.DEFAULT');
+        expect(response.inputs[1].valueType).toEqual('java.lang.String');
+        expect(response.inputs[1].enabled).toEqual(true);
+        expect(response.inputs[1].required).toEqual(true);
+        expect(response.inputs[1].name).toEqual('kubernetesSpace');
+        expect(response.inputs[2].class).toEqual('UIInput');
+        expect(response.inputs[2].inputType).toEqual('org.jboss.forge.inputType.DEFAULT');
+        expect(response.inputs[2].valueType).toEqual('java.lang.String');
+        expect(response.inputs[2].enabled).toEqual(true);
+        expect(response.inputs[2].required).toEqual(false);
+        expect(response.inputs[2].name).toEqual('labelSpace');
+        done();
+      });
+  });
+
+  it('Step_3_1 - import wizard: validate select pipeline process', done => {
+    // given
+    provider.addInteraction({
+      state: 'step3.1.validate',
+      uponReceiving: 'step3.1',
+      withRequest: {
+        method: 'POST',
+        path: '/forge/commands/fabric8-import-git/validate'
+      },
+      willRespondWith: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
+        body: step_3_1_output
+      }
+    }).then(() => done(), function(err) { console.log('failed'); done.fail(err); });
+    const log = () => { };
+    mockLog.createLoggerDelegate.and.returnValue(log);
+    mockAuthService.getToken.and.returnValue('SSO_TOKEN');
+    mockAuthService.isLoggedIn.and.returnValue(true);
+    // when
+    fabric8ForgeService.PostCommand('http://localhost:1234/forge/commands/fabric8-import-git/validate', step_3_1_input)
+      .subscribe((data: any) => {
+        // then
+        const response = data.payload.data;
+        expect(response.state.valid).toEqual(true);
+        expect(response.state.canMoveToNextStep).toEqual(true);
+        expect(response.state.canMoveToPreviousStep).toEqual(true);
+        expect(response.state.steps).toContain('io.fabric8.forge.generator.github.GithubImportPickOrganisationStep');
+        expect(response.state.steps).toContain('io.fabric8.forge.generator.github.GithubImportPickRepositoriesStep');
+        expect(response.state.steps).toContain('Obsidian: Configure Pipeline');
+        expect(response.state.steps).toContain('io.fabric8.forge.generator.kubernetes.CreateBuildConfigStep');
+        expect(response.messages.length).toEqual(0);
+        done();
+      });
+  });
+
+  it('Step_3_2 - import wizard: next select pipeline process', done => {
+    // given
+    provider.addInteraction({
+      state: 'step3.2.next',
+      uponReceiving: 'step3.2',
+      withRequest: {
+        method: 'POST',
+        path: '/forge/commands/fabric8-import-git/next'
+      },
+      willRespondWith: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
+        body: step_3_2_output
+      }
+    }).then(() => done(), function(err) { console.log('failed'); done.fail(err); });
+    const log = () => { };
+    mockLog.createLoggerDelegate.and.returnValue(log);
+    mockAuthService.getToken.and.returnValue('SSO_TOKEN');
+    mockAuthService.isLoggedIn.and.returnValue(true);
+    // when
+    fabric8ForgeService.PostCommand('http://localhost:1234/forge/commands/fabric8-import-git/validate', step_3_2_input)
+      .subscribe((data: any) => {
+        // then
+        const response = data.payload.data;
+        expect(response.state.valid).toEqual(true);
+        expect(response.state.canMoveToNextStep).toEqual(true);
+        expect(response.state.canMoveToPreviousStep).toEqual(true);
+        expect(response.state.steps).toContain('io.fabric8.forge.generator.github.GithubImportPickOrganisationStep');
+        expect(response.state.steps).toContain('io.fabric8.forge.generator.github.GithubImportPickRepositoriesStep');
+        expect(response.state.steps).toContain('Obsidian: Configure Pipeline');
+        expect(response.state.steps).toContain('io.fabric8.forge.generator.kubernetes.CreateBuildConfigStep');
+        expect(response.messages.length).toEqual(0);
+        expect(response.inputs[0].class).toEqual('UISelectOne');
+        expect(response.inputs[0].inputType).toEqual('org.jboss.forge.inputType.DEFAULT');
+        expect(response.inputs[0].valueType).toEqual('java.lang.String');
+        expect(response.inputs[0].enabled).toEqual(true);
+        expect(response.inputs[0].required).toEqual(true);
+        expect(response.inputs[0].name).toEqual('jenkinsSpace');
+        expect(response.inputs[1].class).toEqual('UIInput');
+        expect(response.inputs[1].inputType).toEqual('org.jboss.forge.inputType.DEFAULT');
+        expect(response.inputs[1].valueType).toEqual('java.lang.Boolean');
+        expect(response.inputs[1].enabled).toEqual(true);
+        expect(response.inputs[1].required).toEqual(false);
+        expect(response.inputs[1].name).toEqual('triggerBuild');
+        expect(response.inputs[1].value).toEqual(true);
+        expect(response.inputs[2].class).toEqual('UIInput');
+        expect(response.inputs[2].inputType).toEqual('org.jboss.forge.inputType.DEFAULT');
+        expect(response.inputs[2].valueType).toEqual('java.lang.Boolean');
+        expect(response.inputs[2].enabled).toEqual(true);
+        expect(response.inputs[2].required.toEqualfalse);
+        expect(response.inputs[2].name).toEqual('addCIWebHooks');
+        expect(response.inputs[2].value).toEqual(true);
+        done();
+      });
+
+    // TODO step4
+  });
+
 });
 
---
+++ src/tests/forge-api/import-wizard/step_4_2_output.ts
@@ -0,0 +1,33 @@
+export const step_4_2_output = [
+  null,
+  null,
+  null,
+  null,
+  {
+    'warnings': []
+  },
+  null,
+  {
+    'namespace': 'ckrych',
+    'buildConfigName': 'sprint135bis',
+    'gitUrl': 'https://github.com/corinnekrych/sprint135.git',
+    'cheStackId': 'vert.x',
+    'organisationJenkinsJobUrl': null,
+    'gitRepositoryNames': [
+      'sprint135',
+      'sprint135bis'
+    ],
+    'gitRepositories': [
+      {
+        'url': 'https://github.com/corinnekrych/sprint135.git',
+        'repoName': 'sprint135'
+      },
+      {
+        'url': 'https://github.com/corinnekrych/sprint135bis.git',
+        'repoName': 'sprint135bis'
+      }
+    ],
+    'gitOwnerName': 'corinnekrych',
+    'warnings': []
+  }
+];
